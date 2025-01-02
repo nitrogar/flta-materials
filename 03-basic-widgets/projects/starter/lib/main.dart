6c59a70493cdf5251a295eb5fa2b699236b79921
nitrogar <53944555+nitrogar@users.noreply.github.com>
2025-01-02 23:24:54 +0300

    Update main.dart

--- 03-basic-widgets/projects/starter/lib/main.dart
+++ 03-basic-widgets/projects/starter/lib/main.dart
@@ -1,50 +1,70 @@
 import 'package:flutter/material.dart';
+import 'components/color_button.dart';
+import 'components/theme_button.dart';
+import 'constants.dart';
+import 'home.dart';
 
 void main() {
   // 1
-  runApp(const Yummy());
+  runApp(Yummy());
 }
 
-class Yummy extends StatelessWidget {
+class Yummy extends StatefulWidget {
   // TODO: Setup default theme
 
   // 2
-  const Yummy({super.key});
+  Yummy({super.key});
 
   // TODO: Add changeTheme above here
+  @override
+  State<StatefulWidget> createState() => _YummyState();
+}
 
+class _YummyState extends State<Yummy> {
+
+  // TODO: Setup default theme
+  ThemeMode theme_mode = ThemeMode.light;
+  ColorSelection color_selected = ColorSelection.pink;
+
+  void changeThemeMode(bool useLightMode){
+    setState(() {
+        theme_mode = useLightMode ? ThemeMode.light : ThemeMode.dark;
+    });
+  }
+  void changeColor(int value){
+    setState(() {
+        color_selected  = ColorSelection.values[value];
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
-    const appTitle = 'Yummy';
-
-    // TODO: Setup default theme
-
+    const appTitle = 'Yummy title';
     //3
     return MaterialApp(
       title: appTitle,
       //debugShowCheckedModeBanner: false, // Uncomment to remove Debug banner
 
       // TODO: Add theme
-
+      theme: ThemeData(
+        colorSchemeSeed: color_selected.color,
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: color_selected.color,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      themeMode: theme_mode,
       // TODO: Apply Home widget
 
       // 4
-      home: Scaffold(
-        appBar: AppBar(
-          // TODO: Add action buttons
-          elevation: 4.0,
-          title: const Text(
-            appTitle,
-            style: TextStyle(fontSize: 24.0),
-          ),
-        ),
-        body: const Center(
-          child: Text(
-            'You Hungry?😋',
-            style: TextStyle(fontSize: 30.0),
-          ),
-        ),
-      ),
-    );
+      home: Home(
+        changeTheme: changeThemeMode,
+        changeColor: changeColor,
+        color_selected: color_selected
+    )
+  );
   }
 }
